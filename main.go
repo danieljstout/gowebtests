373664a7f3823c5d6a6175b68650c46903635aba
Daniel Stout <ospreythirtyone@protonmail.com>
2021-02-14 10:53:48 -0500

    Get some headers and body stuff

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func serveHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func logRequests(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "These are the Headers:\n")
+
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+
+	fmt.Fprintf(w, "Here's the body:\n")
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	newStr := buf.String()
+	fmt.Fprintf(w, newStr)
 }
 
 func main() {
 	fmt.Println("Listening at port 8080. Try http://localhost:8080/dickbag")
-	http.HandleFunc("/", serveHello)
+	http.HandleFunc("/", logRequests)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
